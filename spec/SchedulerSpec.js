bde0a9e889917c81015263edd7b663d05ba3083d
lawsons <lawsons@lawsons-HP-Z800-Workstation>
2013-03-13 13:12:57 -0400

    Further progress.

--- spec/SchedulerSpec.js
+++ spec/SchedulerSpec.js
@@ -4,7 +4,7 @@ describe("Scheduler", function() {
 
 	beforeEach(function() {
 		scheduler = new Scheduler({
-			debug: true
+			debug : true
 		});
 	});
 
@@ -20,7 +20,7 @@ describe("Scheduler", function() {
 		var fired = false;
 		var interval = 500;
 		var jobName = "test";
-		
+
 		var callback = function() {
 			fired = true;
 		};
@@ -30,15 +30,45 @@ describe("Scheduler", function() {
 			interval : interval,
 			fn : callback
 		});
-		
+
 		waitsFor(function() {
 			return fired;
 		}, 'Job never fired', interval * intervalWaitMultiplier);
-		
+
 		runs(function() {
-			scheduler.removeJob(jobName);
-			
+			var success = scheduler.removeJob(jobName);
+
 			expect(fired).toEqual(true);
+			expect(success).toEqual(true);
+		});
+	});
+
+	it('can add a job and call multiple callbacks', function() {
+		var fired = 0;
+		var interval = 500;
+		var jobName = "test";
+
+		var callbacks = [ function() {
+			fired++;
+		}, function() {
+			fired++;
+		} ];
+
+		scheduler.addJob({
+			name : jobName,
+			interval : interval,
+			fn : callbacks
+		});
+
+		waitsFor(function() {
+			return fired === 2;
+		}, 'Job never fired', interval * intervalWaitMultiplier);
+
+		runs(function() {
+			var success = scheduler.removeJob(jobName);
+
+			expect(fired).toEqual(2);
+			expect(success).toEqual(true);
 		});
 	});
 });
--- src/Scheduler.js
+++ src/Scheduler.js
@@ -18,21 +18,23 @@
 }(this, function(options) {
 
 	var debug = options.debug || false;
-	
+
 	var log = function(str) {
-		if(debug && console && console.log) {
+		if (debug && console && console.log) {
 			console.log(str);
 		}
 	};
-	
+
 	/*
 	 * Job mapping.
+	 * 
+	 * Job Name => Job
 	 */
 	var jobs = {};
 
-	/*
-	 * Job constructor function.
-	 */
+	/*-----------------
+	 * Job constructor
+	 *-----------------*/
 	var Job = function(options) {
 		// Validation
 		if (typeof options.name === 'undefined') {
@@ -58,9 +60,9 @@
 		this.start(true);
 	};
 
-	/*
-	 * Job Prototype.
-	 */
+	/*---------------
+	 * Job Prototype
+	 *---------------*/
 	Job.prototype = {
 
 		// Job state constants.
@@ -72,7 +74,7 @@
 		},
 
 		name : "",
-		
+
 		// Current state.
 		state : 1,
 
@@ -90,9 +92,9 @@
 
 		timerHandle : null,
 
-		timerHandler : function() {
+		execute : function() {
 			log("Running job: " + this.name);
-			
+
 			this.state = this.STATES.RUNNING;
 
 			for ( var i = 0, len = this.fns.length; i < len; i++) {
@@ -107,53 +109,32 @@
 
 		pause : function() {
 			log("Pausing job: " + this.name);
-			
+
 			this.timerHandle && clearInterval(this.timerHandle);
 			this.state = this.STATES.PAUSED;
 		},
 
 		stop : function() {
 			log("Stopping job: " + this.name);
-			
+
 			this.timerHandle && clearInterval(this.timerHandle);
 		},
 
 		start : function(immediate) {
 			log("Starting job: " + this.name);
-			
+
 			var that = this;
 			this.timerHandle = setInterval(function() {
-				that.timerHandler();
+				that.execute();
 			}, this.interval);
-			
+
 			this.state = this.STATES.SCHEDULED;
-			this.immediate && this.timerHandler();
+			this.immediate && this.execute();
 		}
 	};
 
-	/**
-	 * Add a job.
-	 * 
-	 * @param options.name
-	 *            Job name
-	 * 
-	 * @param options.interval
-	 *            Interval How often the job should run
-	 * 
-	 * @param options.fn
-	 *            Single function reference or array of references to be called
-	 *            when the timer fires.
-	 * 
-	 * @param options.immediate
-	 *            Execute the function immediately.
-	 * 
-	 * @param options.persist
-	 *            Prevents job from being paused via pauseAllJobs() or
-	 *            removeAllJobs() methods.
-	 * 
-	 * @param replace
-	 *            If a job by the name already exists, then replace it. Defaults
-	 *            to false.
+	/*
+	 * See JSDoc below.
 	 */
 	var addJob = function(options, replace) {
 		var name = options.name;
@@ -161,69 +142,69 @@
 		if (typeof jobs[name] === 'undefined' || replace) {
 			jobs[name] = new Job(options);
 		} else {
-			// Not sure I should throw an error here.
-			throw Error("Job name " + name + " is already added.");
+			log("Job name " + name + " is already added.");
 		}
 	};
 
-	/**
-	 * Removes a job by name.
-	 * 
-	 * @param name Job name to remove
+	/*
+	 * See JSDoc below.
 	 */
 	var removeJob = function(name) {
 		var job = jobs[name];
-		
+
 		if (typeof job !== 'undefined') {
 			job.stop();
-			
+
 			log("Removing job: " + name);
-			
+
 			delete jobs[name];
-			
+
 			return true;
-		} 
+		}
 
 		return false;
 	};
 
-	/**
-	 * 
+	/*
+	 * See JSDoc below.
 	 */
 	var runJob = function(name) {
-
+		var job = jobs[name];
+		job && job.execute();
 	};
 
-	/**
-	 * 
+	/*
+	 * See JSDoc below.
 	 */
 	var pauseJob = function(name) {
-
+		var job = jobs[name];
+		job && job.pause();
 	};
 
-	/**
-	 * 
+	/*
+	 * See JSDoc below.
 	 */
 	var resumeJob = function(name) {
-
+		var job = jobs[name];
+		job && job.start();
 	};
 
-	/**
-	 * 
+	/*
+	 * See JSDoc below.
 	 */
 	var removeAllJobs = function() {
 
 	};
 
-	/**
-	 * 
+	/*
+	 * See JSDoc below.
 	 */
 	var pauseAllJobs = function() {
 
 	};
 
-	/**
-	 * 
+	/*
+	 * See JSDoc below.
 	 */
 	var resumeAllJobs = function() {
 
@@ -233,8 +214,40 @@
 	 * Public API
 	 */
 	return {
+		/**
+		 * Add a job.
+		 * 
+		 * @param options.name
+		 *            Job name
+		 * 
+		 * @param options.interval
+		 *            Interval How often the job should run
+		 * 
+		 * @param options.fn
+		 *            Single function reference or array of references to be called
+		 *            when the timer fires.
+		 * 
+		 * @param options.immediate
+		 *            Execute the function immediately.
+		 * 
+		 * @param options.persist
+		 *            Prevents job from being paused via pauseAllJobs() or
+		 *            removeAllJobs() methods.
+		 * 
+		 * @param replace
+		 *            If a job by the name already exists, then replace it. Defaults
+		 *            to false.
+		 */
 		addJob : addJob,
+		
+		/**
+		 * Removes a job by name.
+		 * 
+		 * @param name
+		 *            Job name to remove
+		 */
 		removeJob : removeJob,
+		
 		runJob : runJob,
 		pauseJob : pauseJob,
 		resumeJob : resumeJob,
